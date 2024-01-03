397d902de4c5401bcd173b0b7d773bab0d1c4e49
laxmi <laxmibist009@gmail.com>
2024-01-03 22:36:33 +0545

    feat: Implement Parallax Scroll Gallery with Stylish Design
    Introduce a captivating parallax scroll effect to showcase a dynamic image gallery.
    Utilizes Framer Motion for smooth animations and Lenis for optimal performance.
    Enhance visual appeal with layered images responding to user scroll.
    Style components with modular SCSS in 'page.module.scss' for a sleek and polished design.

--- app/page.js
+++ app/page.js
@@ -1,113 +1,109 @@
-import Image from 'next/image'
+// Import necessary dependencies and styles
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./page.module.scss";
+import Image from "next/image";
+import Lenis from "@studio-freight/lenis";
+import { useTransform, useScroll, motion } from "framer-motion";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+// Define Images data
+const images = [
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
+    "11.jpg",
+    "9.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
+    "10.jpg",
+    "11.jpg",
+    "9.jpg",
+];
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+// Define column
+const Column = ({ images, y }) => {
+    // Render each image within a motion.div with y-axis motion
+    return (
+        <motion.div className={styles.column} style={{ y }}>
+            {images.map((src, i) => {
+                return (
+                    <div key={i} className={styles.imageContainer}>
+                        <Image src={`/img/${src}`} alt="image" fill />
+                    </div>
+                );
+            })}
+        </motion.div>
+    );
+};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+// Define Parallax scroll
+const ParallaxScroll = () => {
+    // Create a reference for the gallery div
+    const gallery = useRef(null);
+    // Create state for storing the dimensions of the window
+    const [dimension, setDimension] = useState({ width: 0, height: 0 });
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    // Destructure properties from the useScroll hook
+    const { scrollYProgress } = useScroll({
+        target: gallery,
+        offset: ["start end", "end start"],
+    });
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    // Destructure the height property from the dimension state
+    const { height } = dimension;
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+    // Create motion values for animation based on scrollYProgress
+    const y = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
+    const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
+    const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
+    const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3]);
+
+    // useEffect hook for initializing Lenis, handling resize, and cleanup
+    useEffect(() => {
+        // Create a new Lenis instance
+        const lenis = new Lenis();
+
+        // Define a RAF function for animation
+        const raf = (time) => {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        };
+
+        // Define a resize function to update dimensions on window resize
+        const resize = () => {
+            setDimension({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+
+        // Add event listeners for resize and initial resize
+        window.addEventListener("resize", resize);
+        requestAnimationFrame(raf);
+        resize();
+
+        // Cleanup function to remove event listener on component unmount
+        return () => {
+            window.removeEventListener("resize", resize);
+        };
+    }, []);
+
+    return (
+        <main className={styles.main}>
+            <div className={styles.spacer}></div>
+            <div ref={gallery} className={styles.gallery}>
+                <Column images={[images[0], images[1], images[2]]} y={y} />
+                <Column images={[images[3], images[4], images[5]]} y={y2} />
+                <Column images={[images[6], images[7], images[8]]} y={y3} />
+                <Column images={[images[9], images[10], images[11]]} y={y4} />
+            </div>
+            <div className={styles.spacer}></div>
+        </main>
+    );
+};
+
+export default ParallaxScroll;
